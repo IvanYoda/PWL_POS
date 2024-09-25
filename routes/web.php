43f94996f02704b3140dbe9ad793212593593660
IvanYoda <ivanyodaazul@gmail.com>
2024-09-26 01:44:58 +0700

    Js 4

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,13 @@ Route::get(uri: '/kategori',action: [kategoriController :: class, 'index']);
 
 Route::get(uri: '/user',action: [kategoriController :: class, 'index']);
 Route::get(uri: '/user',action: [userController :: class, 'index']);
+
+Route::get(uri: '/user/tambah', action: [userController::class, 'tambah']);
+
+Route::post(uri: '/user/tambah_simpan', action: [userController :: class, 'tambah_simpan']);
+
+Route :: get(uri: '/user/ubah/{id}', action: [userController :: class, 'ubah']);
+
+Route :: get(uri: '/user/ubah_simpan/{id}', action: [userController :: class, 'ubah_simpan']);
+
+Route:: get(uri: '/user/hapus/{id}', action: [userController :: class, 'hapus']);
