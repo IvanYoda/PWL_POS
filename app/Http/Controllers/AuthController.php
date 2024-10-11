b2806ddb3d44e939fe92a3e93841f4f853b8f9e8
IvanYoda <ivanyodaazul@gmail.com>
2024-10-11 11:11:17 +0700

    Tugas 4 Js 7

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,7 +1,14 @@
 <?php
+
 namespace App\Http\Controllers;
+
+use App\Models\LevelModel;
+use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
 class AuthController extends Controller
 {
     public function login()
@@ -9,12 +16,15 @@ class AuthController extends Controller
         if (Auth::check()) { // jika sudah login, maka redirect ke halaman home
             return redirect('/');
         }
+
         return view('auth.login');
     }
+
     public function postlogin(Request $request)
     {
         if ($request->ajax() || $request->wantsJson()) {
             $credentials = $request->only('username', 'password');
+
             if (Auth::attempt($credentials)) {
                 return response()->json([
                     'status' => true,
@@ -22,18 +32,68 @@ class AuthController extends Controller
                     'redirect' => url('/')
                 ]);
             }
+
             return response()->json([
                 'status' => false,
                 'message' => 'Login Gagal'
             ]);
         }
+
         return redirect('login');
     }
+
     public function logout(Request $request)
     {
         Auth::logout();
+
         $request->session()->invalidate();
         $request->session()->regenerateToken();
+
         return redirect('login');
+    }
+
+
+    public function register()
+    {
+        $level = LevelModel::select('level_id', 'level_nama')->get();
+
+        return view('auth.register')->with('level', $level);
+    }
+
+    public function postRegister(Request $request)
+    {
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'level_id' => 'required|integer',
+                'username' => 'required|string|min:3|unique:m_user,username',
+                'nama' => 'required|string|max:100',
+                'password' => 'required|min:5'
+            ];
+
+            $validator = Validator::make($request->all(), $rules);
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validasi Gagal',
+                    'msgField' => $validator->errors(),
+                ]);
+            }
+
+            // Hash password sebelum disimpan
+            $data = $request->all();
+            $data['password'] = Hash::make($request->password);
+
+            // Simpan data user
+            usermodel::create($data);
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Data user berhasil disimpan',
+                'redirect' => url('login') // Redirect ke halaman login
+            ]);
+        }
+
+        // Jika bukan AJAX, arahkan ke halaman login
+        return redirect('login')->with('success', 'Registrasi berhasil!');
     }
 }
